dd0e983440e585667c06cec3825e6e90ec91cf45
Dawid Paśko <dawpasko@gmail.com>
2024-12-29 23:18:26 +0100

    AboutUs page lay out

--- EKsite/src/pages/AboutPage/About.jsx
+++ EKsite/src/pages/AboutPage/About.jsx
@@ -1,12 +1,42 @@
 import NavBar from "../../components/NavBar"
-
+import Background from "../../assets/images/EKbackground.png"
 const About = () => {
   return (
     <>
-    <div className="aboutContainer">
+    <div className="aboutContainer" style={{backgroundImage: `url(${Background})`,}}>
      <NavBar/>  
-     <div className="">
-      
+     <h1>Rodzina</h1>
+     <div className="firstAboutContainer">
+      <dir className="videoContainer"></dir>
+      <div className="descriptionContainer">
+      </div>
+     </div>
+     <div className="secondContainer">
+      <div className="secondDescriptionContainer">
+       <div className="descriptionContainer">
+          <div className="description"><h1></h1></div>
+          <div className="img"></div>
+       </div>
+       <div className="descriptionContainer">
+          <div className="description"><h1></h1></div>
+          <div className="img"></div>
+       </div>
+       <div className="descriptionContainer">
+          <div className="description"><h1></h1></div>
+          <div className="img"></div>
+       </div>
+      </div>
+     </div>
+     <div className="thirdContainer">
+      <h1>Ask Us</h1>
+      <div className="askUsContainer"></div>
+      <h2>Social Media</h2>
+      <div className="socialMediaContainer">
+        <div className="icon"></div>
+        <div className="icon"></div>
+        <div className="icon"></div>
+        <div className="icon"></div>
+      </div>
      </div>
     </div>   
     </>
