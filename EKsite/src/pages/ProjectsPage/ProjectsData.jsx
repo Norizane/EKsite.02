c85f015a29b71d47e72408d5c2662ce76745b7cf
Dawid Paśko <dawpasko@gmail.com>
2025-01-17 00:17:40 +0100

    Adding photos to the section and descriptions

--- EKsite/src/pages/ProjectsPage/ProjectsData.jsx
+++ EKsite/src/pages/ProjectsPage/ProjectsData.jsx
@@ -1,40 +1,43 @@
-import photo from "../../assets/images/GrupPhotos/ekwador4.jpg"
-import photo2 from "../../assets/images/Ekwdor2000.jpg"
+import instagram from "../../assets/images/ProjectsImages/Instagram.jpg"
+import art from "../../assets/images/ProjectsImages/MoskaArt.png"
+import random from "../../assets/images/ProjectsImages/Ekwdor2000.jpg"
+import ekwadorMixtape from "../../assets/images/ProjectsImages/PłytaEK.png"
+import discord from "../../assets/images/ProjectsImages/Discord.jpg"
 
 const projectsData = [
   {
     "id": 1,
     "title": "Ekwador mixtape",
-    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    "image": photo,
+    "description": "Years ago, we started as a group of dreamers, writing poetry and sharing ideas. Now, we've turned those memories into an album—original songs and custom artwork that celebrate our beginnings and bring our old verses to life. Discover the story behind the music.",
+    "image": ekwadorMixtape,
     "link": "https://linkdoprojektu3.com"
   },
   {
     "id": 2,
     "title": "Art",
-    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    "image": photo,
+    "description": "From delicate sketches to vivid digital creations, our art is a window into a world of imagination. Each piece holds a story, waiting to be uncovered. Dive into the mystery and see where the journey takes you.",
+    "image": art,
     "link": "https://linkdoprojektu3.com"
   },
   {
     "id": 3,
     "title": "Instagram",
-    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    "image": photo,
+    "description": "We've joined Instagram to share our journey and connect with our community. Each post is a memory, a keepsake, and a way to celebrate the moments that define us. Follow along and be part of our story!",
+    "image": instagram,
     "link": "https://linkdoprojektu3.com"
   },
   {
     "id": 4,
     "title": "Discord",
-    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    "image": photo,
+    "description": "Discord is where our creativity thrives—a virtual meeting spot where exciting projects take shape and unforgettable moments are shared. It’s not just for brainstorming; it’s where we game, laugh, and connect with those who can’t join us in person. Dive in and be part of the magic!",
+    "image": discord,
     "link": "https://linkdoprojektu3.com"
   },
   {
     "id": 5,
     "title": "Upcoming",
     "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    "image": photo2,
+    "image": random,
   }
 ];
 
